f6d25f4218e94de29785d8e0574f4fd369838f0b
trungnguyen10 <32494880+trungnguyen10@users.noreply.github.com>
2018-02-20 19:52:39 -0600

    Update MagicSquare.cpp

--- MagicSquare.cpp
+++ MagicSquare.cpp
@@ -1,9 +1,21 @@
 #include <iostream>
 using namespace std;
 
+bool Is_odd(int);
+
 int main()
 {
   int input;
+  do
+  {
+    cout << "Please enter a positive odd number: ";
+    getline(cin,input);
+    if (!Is_odd(input))
+    {
+      cout << "Your number is invalid." << endl;
+    }
+  }while (!Is_ood(input));
+  
   
   
   return 0;
